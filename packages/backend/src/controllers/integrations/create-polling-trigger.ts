6a77f3a7abff32566beea81d65d3a85a4e0cecd9
moaaz <moaz.elsayed002@gmail.com>
2023-08-31 20:45:50 +0000

    feat: add dynamic triggers

---
+++ packages/backend/src/controllers/integrations/create-polling-trigger.ts
@@ -0,0 +1,91 @@
+import { Response } from 'express';
+import { IRequest } from '@automatisch/types';
+import fs from 'fs-extra';
+import appendImportToFile from './add-import';
+import appendModuleToExport from './add-export';
+
+function generateTriggerFile(triggerConfig: any): string {
+  const { name, key, pollInterval, description, run } = triggerConfig;
+
+  const triggerFileContent = `
+import { IJSONObject } from '@automatisch/types';
+import defineTrigger from '../../../../helpers/define-trigger';
+
+export default defineTrigger({
+  name: '${name}',
+  key: '${key}',
+  pollInterval: ${pollInterval},
+  description: '${description}',
+
+  async run($) {
+    ${run}
+  },
+});
+`;
+
+  return triggerFileContent;
+}
+
+async function generateMainIndexFile(appKey: string) {
+  try {
+    const triggerDirectories = await fs.readdir(
+      `./src/apps/${appKey}/triggers`,
+      { withFileTypes: true }
+    );
+
+    const validTriggerDirectories = triggerDirectories
+      .filter((dir) => dir.isDirectory())
+      .map((dir) => dir.name);
+
+    const importStatements = validTriggerDirectories
+      .map((dir) => `import ${dir} from './${dir}';`)
+      .join('\n');
+
+    const exportStatement = `export default [${validTriggerDirectories.join(
+      ', '
+    )}];`;
+
+    return `${importStatements}\n\n${exportStatement}`;
+  } catch (error) {
+    console.error('Error generating main trigger index file:', error);
+    return '';
+  }
+}
+
+export default async (request: IRequest, res: Response) => {
+  const appKey = request.params.appkey;
+
+  try {
+    const triggerConfig = request.body;
+
+    const triggersDirectoryPath = `./src/apps/${appKey}/triggers`;
+    if (!fs.existsSync(triggersDirectoryPath)) {
+      await fs.ensureDir(triggersDirectoryPath);
+      const newImport = `import triggers from './triggers';`;
+      await appendImportToFile(`./src/apps/${appKey}/index.ts`, newImport);
+      await appendModuleToExport(`./src/apps/${appKey}/index.ts`, 'triggers');
+    }
+
+    const triggerFileContent = generateTriggerFile(triggerConfig);
+
+    await fs.ensureDir(`./src/apps/${appKey}/triggers`);
+    const triggerDir = `./src/apps/${appKey}/triggers/${triggerConfig.key}`;
+    await fs.ensureDir(triggerDir);
+    await fs.writeFile(`${triggerDir}/index.ts`, triggerFileContent, 'utf-8');
+
+    const mainIndexFileContent = await generateMainIndexFile(appKey);
+
+    await fs.writeFile(
+      `./src/apps/${appKey}/triggers/index.ts`,
+      mainIndexFileContent,
+      'utf-8'
+    );
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(
+      `Error creating/updating triggers for app ${appKey}: ${error}`
+    );
+    res.status(500).send('Internal Server Error');
+  }
+};
--- packages/backend/src/routes/integrations.ts
+++ packages/backend/src/routes/integrations.ts
@@ -5,6 +5,7 @@ import appConfig from '../config/app';
 import createApp from '../controllers/integrations/create-app';
 import createAuth from '../controllers/integrations/create-auth';
 import uniqueApp from '../controllers/integrations/unique-app';
+import createTrigger from '../controllers/integrations/create-polling-trigger';
 const router = Router();
 
 router.use(
@@ -18,5 +19,6 @@ router.use(
 
 router.post('/create', uniqueApp, createApp);
 router.post('/auth/apikey/:appkey', createAuth);
+router.post('/trigger/polling/:appkey', createTrigger);
 
 export default router;
